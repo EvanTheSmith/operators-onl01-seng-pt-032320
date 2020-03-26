f5d90198a14e3daedc690297c134acf39d25ab80
Evan Smith <professionalemail@zoho.com>
2020-03-26 03:07:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,15 @@
 def unsafe?(speed)
   if speed<40||speed>60
-    answer=true
+    true
   else
-    answer=false
+    false
   end
 end
 
 
 
 def not_safe?(speed)
-	speed<40||speed>60 ? answer=true : answer=false
+	speed<40||speed>60 ? true : false
 end
 	
 
